e0fb6a2eafbbb4ebe9cbd140bfb9061995f6c4df
Jason <jzhao.690@gmail.com>
2015-05-14 17:26:23 -0700

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+
   def home
+    #rails will execute the code in the home action, and then render the view corresponding to the action
   end
 
   def help
+  end
+
+  def about
   end
 end
