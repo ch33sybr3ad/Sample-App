f3bd14ce9ca3eb2b3541811557035fd8f844f642
Jason <jzhao.690@gmail.com>
2015-06-11 10:27:42 -0700

    add create post view, tests

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
   def home
-    #rails will execute the code in the home action, and then render the view corresponding to the action
+    @micropost = current_user.microposts.build if logged_in?
   end
 
   def help
